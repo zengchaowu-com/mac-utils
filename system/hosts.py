ed902f76c6fe5d9f2b0ce298a68bbe36a5094033
zengchaowu-com <admin@zengchaowu.com>
2020-09-04 17:36:03 +0800

    mmm

--- system/hosts.py
+++ system/hosts.py
@@ -12,4 +12,13 @@ def raw_githubusercontent_com():
         sudo chmod 755 /etc/hosts
         """)
 
+def api_github_com():
+    records=os.popen("cat /etc/hosts | grep api.github.com")
+    if len(records.readlines()) == 0 :
+        os.system("""
+        sudo chmod 777 /etc/hosts
+        sudo echo 13.250.94.254 api.github.com >> /etc/hosts
+        sudo chmod 755 /etc/hosts
+        """)
+
 raw_githubusercontent_com()
