ea606241e1768a50730c82762751b70ca3c3a971
zengchaowu-com <admin@zengchaowu.com>
2020-09-04 16:58:07 +0800

    mmm

---
+++ applications/homebrew.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import os
+
+def dependencies()
+    pass
+
+def check():
+    return False
+
+def install():
+    os.system("brew install tree")
+    pass
+
+def uninstall():
+    pass
+
+print(check())
