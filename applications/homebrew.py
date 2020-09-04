72b2a01c703c4658c1d706c4f7660a0176587607
zengchaowu-com <admin@zengchaowu.com>
2020-09-04 17:30:45 +0800

    mmm

--- applications/homebrew.py
+++ applications/homebrew.py
@@ -2,17 +2,23 @@
 # coding: utf-8
 
 import os
+import homebrew
 
 def dependencies():
     pass
 
 def check():
-    # return False
-    pass
+    path = os.popen("which brew")
+    return len(path.readlines()) > 0
 
 def install():
-    # os.system("brew install tree")
-    pass
+    if not check():
+        os.system("brew install wget")
 
 def uninstall():
-    pass
+    if check():
+        os.system("brew uninstall wget && rm -fr /usr/local/etc/wgetrc")
+
+check()
+install()
+uninstall()
