05e2d27f5c8e4763216fcdf2476f70ad672e90f8
zengchaowu-com <admin@zengchaowu.com>
2020-09-04 11:10:50 +0800

    mmm

--- init.sh
+++ init.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # 初始化系统 此脚本用于第一次初始化电脑
 
+# 修改Hosts
+function update_hosts()
+{
+    echo "修改Hosts文件"
+    sudo chmod 777 /etc/hosts
+
+    record=`cat /etc/hosts | grep raw.githubusercontent.com`
+    if [ -z $record ];then
+        sudo echo 151.101.108.133 raw.githubusercontent.com >> /etc/hosts
+    fi
+
+    record=`cat /etc/hosts | grep api.github.com`
+    if [ -z $record ];then
+        sudo echo 13.250.94.254 api.github.com >> /etc/hosts
+    fi
+    
+    sudo chmod 755 /etc/hosts
+}
+
+
 # 先判断Xcode是否已安装
 xcode_path=/Applications/Xcode.app
 if  [ -d $xcode_path ];then
@@ -8,13 +28,3 @@ if  [ -d $xcode_path ];then
 else
     echo "请先安装Xcode"
 fi
-
-# 修改Hosts
-function update_hosts()
-{
-    echo "修改Hosts文件"
-    sudo chmod 777 /etc/hosts
-    sudo echo 151.101.108.133 raw.githubusercontent.com >> /etc/hosts
-    sudo echo 13.250.94.254 api.github.com >> /etc/hosts
-    sudo chmod 755 /etc/hosts
-}
