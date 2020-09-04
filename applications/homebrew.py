f70465e6ccf43e31d19ae09ebf255121486197e3
zengchaowu-com <admin@zengchaowu.com>
2020-09-04 17:31:50 +0800

    mmm

--- applications/homebrew.py
+++ applications/homebrew.py
@@ -2,20 +2,24 @@
 # coding: utf-8
 
 import os
-import homebrew
+import system.gem_sources
 
 def dependencies():
+    gem_sources.ruby_china()
     pass
 
 def check():
+    dependencies()
     path = os.popen("which brew")
     return len(path.readlines()) > 0
 
 def install():
+    dependencies()
     if not check():
         os.system("brew install wget")
 
 def uninstall():
+    dependencies()
     if check():
         os.system("brew uninstall wget && rm -fr /usr/local/etc/wgetrc")
 
